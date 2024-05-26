23006e343cf172debed3ddc6b4ba2f6f515183b1
KacDabQ <u118_kacdab_waw@technischools.com>
2024-05-26 19:08:22 +0200

    Initial

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+let debug_console = document.getElementById("debug_console");
+let board = document.getElementById("board");
+
+for (let c of board.children) {
+  c.addEventListener("click", on_cell_click);
+}
+
+function on_cell_click(e) {
+  log_debug("clicked: " + e.target);
+  e.target.className = "";
+  e.target.classList.add("o");
+}
+
+function log_debug(message) {
+  debug_console.textContent += message + "\n";
+}
